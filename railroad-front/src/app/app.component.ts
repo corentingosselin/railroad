012ab0aa9986b55a6faeeabe2466afc85d7b423f
DrNoProblem <maxverhaeghe@gmail.com>
2024-03-20 16:31:52 +0100

    front ok

--- railroad-front/src/app/app.component.ts
+++ railroad-front/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ConnectWalletComponent } from './connect-wallet/connect-wallet.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ConnectWalletComponent],
+  imports: [RouterOutlet, ConnectWalletComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
